90fd52065486fbc76bac0ec127aac3854046bd96
zhangsan <1113471200@qq.com>
2021-01-27 08:48:48 +0800

    栈学习1.0

---
+++ src/main/java/com/study/play/Play.java
@@ -0,0 +1,14 @@
+package com.study.play;
+
+/**
+ * @author ZhangHao
+ * @version 1.0.0
+ * @ClassName Play
+ * @Description TODO
+ * @createTime 2020年11月18日 22:05:00
+ */
+public class Play {
+    public static void main(String[] args) {
+        System.getProperties().list(System.out);
+    }
+}
