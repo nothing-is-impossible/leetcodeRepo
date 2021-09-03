43ff470fa43e2f861a341ddd3e79f92056e0c795
nothing-is-impossible <1113471200@qq.com>
2021-09-03 17:54:32 +0800

    6

---
+++ src/main/java/com/study/offer/Offer007.java
@@ -0,0 +1,21 @@
+package com.study.offer;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * 给定一个包含 n 个整数的数组nums，判断nums中是否存在三个元素a ，b ，c ，使得a + b + c = 0 ？请找出所有和为 0 且不重复的三元组。
+ *
+ * 输入：nums = [-1,0,1,2,-1,-4]
+ * 输出：[[-1,-1,2],[-1,0,1]]
+ * @author zh
+ */
+public class Offer007 {
+    public List<List<Integer>> threeSum(int[] nums) {
+
+        Arrays.sort(nums);
+        for (int num : nums) {
+
+        }
+    }
+}
